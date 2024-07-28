20fc3a13c636e6ae94ed8232050a5ff194eac547
Santhiyasanthiya <santhiya30032@gmail.com>
2024-07-28 12:20:34 +0530

    28.070

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -26,7 +26,7 @@ const Contact = () => {
     }),
     onSubmit: async (values) => {
       try {
-        const response = await fetch("http://localhost:4000/api/contact", {
+        const response = await fetch("http://localhost:4000/contact", {
           method: "POST",
           headers: {
             "Content-Type": "application/json",
